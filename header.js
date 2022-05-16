235c428673dfcebe28788aa1cc6cef8db29f1dc9
soapboxsciencebrighton <105506808+soapboxsciencebrighton@users.noreply.github.com>
2022-05-16 18:40:10 +0100

    Update header.js

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 fetch('header.html')
 .then(res => res.text())
 .then(text => {
-    let oldelem = document.querySelector("script#header");
+    let oldelem = document.querySelector("script#headerscript");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
