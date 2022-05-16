600fdd7c69ae86caebfd8e63990923a76089f560
soapboxsciencebrighton <105506808+soapboxsciencebrighton@users.noreply.github.com>
2022-05-16 18:33:00 +0100

    Update header.js

--- header.js
+++ header.js
@@ -8,12 +8,3 @@ fetch('header.html')
 })
 
 
- function dropmenu() {
-   var x = document.getElementById("mysidebar");
-   if (x.className === "sidebar") {
-     x.className += " responsive"; 
-   } else {
-     x.className = "sidebar";
-   }
-}
-
