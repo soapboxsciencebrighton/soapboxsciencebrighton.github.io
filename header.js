e2e25c70bf8baa00d288d362b878e07436768c88
soapboxsciencebrighton <105506808+soapboxsciencebrighton@users.noreply.github.com>
2022-05-16 18:26:27 +0100

    Create header.js

---
+++ header.js
@@ -0,0 +1,19 @@
+fetch('header.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#header");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
+
+
+ function dropmenu() {
+   var x = document.getElementById("mysidebar");
+   if (x.className === "sidebar") {
+     x.className += " responsive"; 
+   } else {
+     x.className = "sidebar";
+   }
+}
+
